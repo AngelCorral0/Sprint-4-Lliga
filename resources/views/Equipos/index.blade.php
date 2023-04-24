9f13311be4595ab20332facc5a28c8e7a39ef64a
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-04-24 11:47:03 +0200

    añadido edit view

--- resources/views/Equipos/index.blade.php
+++ resources/views/Equipos/index.blade.php
@@ -3,8 +3,7 @@
 @section('title', 'Home')
 @section('meta-description', 'Home meta description')
 
-@section('content')
-    <!-- component -->
+  
 @section('content')
 <link
 	href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
