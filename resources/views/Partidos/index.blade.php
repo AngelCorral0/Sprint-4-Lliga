4bf33a1a0d7c3a39f1d167afc76265828668ca57
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-04-26 10:19:47 +0200

    eliminado el "!" de crear equipo

--- resources/views/Partidos/index.blade.php
+++ resources/views/Partidos/index.blade.php
@@ -10,7 +10,7 @@
         </div>
         <a href="{{ route('partidos.create') }}">
             <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                Crea un nuevo partido!
+                Crea un nuevo partido
             </button>
         </a>
         <div class="mt-10 inline-block min-w-full shadow rounded-lg overflow-hidden">
